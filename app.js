3b6b2597b2db52a49423f8353cdec4c994d6332a
huzhengquan <huzhengquan@gmail.com>
2016-04-19 16:16:01 +0800

    first

--- app.js
+++ app.js
@@ -6,22 +6,21 @@ const app = require('koa')(),
 
 app.use(require("koa-logger")());
 
-const conf = require('./config');
-
 app.use(function *(next){
 	if(this.method!='POST'){
 		return this.status = 404;
 	}
 	const reqid = this.path.substring(1);
+	const conf = require('./config');
 	if(!conf.tasks.hasOwnProperty(reqid)){
 		return this.status = 404;
 	}
 	console.log('run',conf.tasks[reqid]);
 	exec(conf.tasks[reqid]['command'],function(err, stdout, stderr){
-		console.log('stderr:',stderr);
-		console.log('stdout:',stdout);
+		if(stderr) console.log('command stderr:',stderr);
+		if(stdout) console.log('command stdout:',stdout);
 	});
-	this.body='run';
+	this.body='running';
 });
 
 app.listen(conf.port || 3000);
